5c945325bff5a9c67a75e8691113c75a2ed0b164
Alvaro Mejia <alvaromejia26@gmail.com>
2017-08-31 17:34:46 -0500

    App terminada

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,33 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor(private http:Http) {
     this.cargar_productos();
+  }
+
+  public buscar_producto(termino:string){
+    if(this.productos.length===0){
+      this.cargar_productos().then(()=>{
+        //termino la carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+      this.filtrar_productos(termino);
+    }
+
+  }
+
+  private filtrar_productos(termino:string){
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+    this.productos.forEach(prod=>{
+      if(prod.categoria.indexOf(termino)>=0 || prod.titulo.toLowerCase().indexOf(termino)>=0){
+        this.productos_filtrado.push(prod);
+      }
+    });
   }
 
   public cargar_producto(cod:string){
@@ -17,10 +40,14 @@ export class ProductosService {
 
   public cargar_productos(){
     this.cargando = true;
-    this.http.get("https://paginaweb-1e80d.firebaseio.com/productos_idx.json")
-    .subscribe(res => {
-      this.cargando = false;
-      this.productos = res.json();
-    })
+    let promesa = new Promise((resolve,reject)=>{
+      this.http.get("https://paginaweb-1e80d.firebaseio.com/productos_idx.json")
+      .subscribe(res => {
+        this.cargando = false;
+        this.productos = res.json();
+        resolve();
+      });
+    });
+    return promesa;
   }
 }
