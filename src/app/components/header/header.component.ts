6b42510bc7f48581e375580e6b6b5f234a2f0393
Alvaro Mejia <alvaromejia26@gmail.com>
2017-08-31 14:31:32 -0500

    Pagina de busqueda creada sin funcionar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component} from '@angular/core';
 import { InformacionService } from "../../services/informacion.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor( public _is:InformacionService ){
-
+  constructor( public _is:InformacionService, private router:Router ){}
+    buscar_producto(termino:string){
+      this.router.navigate(['buscar',termino]);
   }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,6 +10,11 @@ export class ProductosService {
   constructor(private http:Http) {
     this.cargar_productos();
   }
+
+  public cargar_producto(cod:string){
+    return this.http.get(`https://paginaweb-1e80d.firebaseio.com/productos/${cod}.json`)
+  }
+
   public cargar_productos(){
     this.cargando = true;
     this.http.get("https://paginaweb-1e80d.firebaseio.com/productos_idx.json")
