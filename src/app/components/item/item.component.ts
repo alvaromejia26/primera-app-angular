4819831e4888b6319dbf0c96764c6419b1d2671c
Alvaro Mejia <alvaromejia26@gmail.com>
2017-09-01 01:16:06 -0500

    Primera versión

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -14,8 +14,6 @@ export class ItemComponent {
 
   constructor(private route:ActivatedRoute, private _ps:ProductosService) {
     route.params.subscribe(parametros=>{
-      console.log(parametros);
-      console.log(parametros['id']);
       _ps.cargar_producto(parametros['id'])
         .subscribe(res=>{
           this.cod = parametros['id'];
